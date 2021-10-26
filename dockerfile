2a95f4a1a9699dbcff0ec9a670e29000fb3c06ef
Sergio Santos <sfr_s@outlook.com>
2021-10-27 00:14:43 +0100

    add entrypoint to application startup

--- dockerfile
+++ dockerfile
@@ -4,5 +4,4 @@ COPY ./src/app/ /usr/src/app
 WORKDIR /usr/src/app
 
 EXPOSE 3000
-RUN npm i
-CMD [ "npm","start" ]
+ENTRYPOINT [ "sh","./helper.sh" ]
---
+++ src/app/helper.sh
@@ -0,0 +1,11 @@
+#update packages
+#!/bin/sh
+echo "Updating packages..."
+npm update
+echo "Packages are successful update!"
+
+#start application
+echo "Starting application..."
+npm start
+
+
